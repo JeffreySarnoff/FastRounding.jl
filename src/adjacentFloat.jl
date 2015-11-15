fdc1830b74882b61e4e82fe07c12efe0182106a4
jeffreysarnoff-dev <jeffreysarnoff1@gmail.com>
2015-11-15 18:15:32 -0500

    Update adjacentFloat.jl

--- src/adjacentFloat.jl
+++ src/adjacentFloat.jl
@@ -3,6 +3,8 @@
   That differs from nextfloat(-Inf) == -realmax(), prevfloat(Inf) == realmax()
   (prevfloat(Inf)==Inf makes more sense to me, and likely is more helpful).
   And steps twice when given values of very small magnitude (see paper).
+  The alternative implementation, converting to [U]Int and adding/subtracting 1,
+  returns NaN when given +/-Inf -- and checking for Inf adds branching. 
   ref:
   'Predecessor and Successor In Rounding To Nearest' by Siegried M. Rump
   "The routines deliver the exact answer except for a small range near underflow,
