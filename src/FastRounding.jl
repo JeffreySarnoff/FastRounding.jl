d7fbd48a950aa1432e1479dc3c29554f32c71d79
jeffreysarnoff-dev <jeffreysarnoff1@gmail.com>
2015-11-15 17:59:18 -0500

    Update FastRounding.jl

--- src/FastRounding.jl
+++ src/FastRounding.jl
@@ -1,17 +1,13 @@
 module FastRounding
 
-export RoundToZero, RoundFromZero, RoundUp, RoundDown
+import (+),(-),(*),(/),(sqrt)
 
 set_rounding(Float64, RoundNearest)
 set_rounding(Float32, RoundNearest)
 set_rounding(Float16, RoundNearest)
 
-include("eftArithmetic.jl")
 include("adjacentFloat.jl")
-
-include("RoundToZero.jl")
-include("RoundFromZero.jl")
-include("RoundUp.jl")
-include("RoundDown.jl")
+include("eftArithmetic.jl")
+include("eftRounding.jl")
 
 end # module
