f864fb2a1b26fac5b172b5377bbbd056033d7f20
jeffreysarnoff-dev <jeffreysarnoff1@gmail.com>
2015-11-15 17:03:08 -0500

    Create RoundToZero.jl

---
+++ src/RoundToZero.jl
@@ -0,0 +1,43 @@
+#=
+       RoundToZero
+       hi  lo  rounding        fastrounding
+       +   +   hi              hi
+       +   -   prevfloat(hi)   nextNearerToZero(hi)
+       -   +   hi              hi
+       -   -   nextfloat(hi)   nextNearerToZero(hi)
+=#
+
+@inline roundingToZero{T<:AbstractFloat}(hi::T,lo::T) = (signbit(lo) ? nextNearerToZero(hi) : hi)
+
+function (+){T<:AbstractFloat}(a::T, b::T, ::Type{RoundingMode{:ToZero}})
+    hi,lo = eftSum2(a,b)
+    roundingToZero(hi, lo)
+end
+
+function (-){T<:AbstractFloat}(a::T, b::T, ::Type{RoundingMode{:ToZero}})
+    hi,lo = eftDiff2(a,b)
+    roundingToZero(hi, lo)
+end
+
+function (*){T<:AbstractFloat}(a::T, b::T, ::Type{RoundingMode{:ToZero}})
+    hi,lo = eftProd2(a,b)
+    roundingToZero(hi, lo)
+end
+
+function (/){T<:AbstractFloat}(a::T, b::T, ::Type{RoundingMode{:ToZero}})
+    hi,lo = eftDiv2Approx(a,b)
+    roundingToZero(hi, lo)
+end
+
+function (square){T<:AbstractFloat}(a::T, b::T, ::Type{RoundingMode{:ToZero}})
+    hi,lo = eftSquare(a,b)
+    roundingToZero(hi, lo)
+end
+
+function (sqrt){T<:AbstractFloat}(a::T, b::T, ::Type{RoundingMode{:ToZero}})
+    hi,lo = eftSqrtApprox(a,b)
+    roundingToZero(hi, lo)
+end
+
+
+
