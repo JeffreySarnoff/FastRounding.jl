3f7a1d692e4d1ead63f56a4afa6443b51da4422d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-07 22:11:22 -0400

    Update FastRounding.jl

--- src/FastRounding.jl
+++ src/FastRounding.jl
@@ -3,8 +3,6 @@ module FastRounding
 using AdjacentFloats
 using ErrorfreeArithmetic
 
-import Base: (+),(-),(*),(/),(sqrt)
-
 set_rounding(Float64, RoundNearest)
 set_rounding(Float32, RoundNearest)
 #set_rounding(Float16, RoundNearest) Julia does not support this in v0.6.0-pre.beta.60
