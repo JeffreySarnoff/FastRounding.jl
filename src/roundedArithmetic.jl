556d76e178c50ac3b88f37168599d5dd12dcf6ce
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-07 21:00:43 -0400

    Update roundedArithmetic.jl

--- src/roundedArithmetic.jl
+++ src/roundedArithmetic.jl
@@ -1,31 +1,36 @@
 
-function (+){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = add_errorfree(a, b)
-    round_errorfree(hi, lo, rounding)
+function (+){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)7
+    hi, lo = add_errorfree(a, b)
+    return round_errorfree(hi, lo, rounding)
 end
 
 function (-){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = subtract_errorfree(a,b)
-    round_errorfree(hi, lo, rounding)
+    hi, lo = subtract_errorfree(a,b)
+    return round_errorfree(hi, lo, rounding)
 end
 
 function (*){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = multiply_errorfree(a, b)
-    round_errorfree(hi, lo, rounding)
+    hi, lo = multiply_errorfree(a, b)
+    return round_errorfree(hi, lo, rounding)
+end
+
+function (inv){T<:AbstractFloat, R<:RoundingMode}(a::T, rounding::R)
+    hi, lo = inv_errorfree(a)
+    return round_errorfree(hi, lo, rounding)
 end
 
 function (/){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = divide_accurately(a, b)
-    round_errorfree(hi, lo, rounding)
+    hi, lo = divide_accurately(a, b)
+    return round_errorfree(hi, lo, rounding)
 end
 
 function (square){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = square_errorfree(a, b)
-    round_errorfree(hi, lo, rounding)
+    hi, lo = square_errorfree(a, b)
+    return round_errorfree(hi, lo, rounding)
 end
 
 function (sqrt){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = sqrt_accurately(a, b)
-    round_errorfree(hi, lo, rounding)
+    hi, lo = sqrt_accurately(a, b)
+    return round_errorfree(hi, lo, rounding)
 end
 
