4b504414dc7b317e5cc3a3f9bb9861077c83984d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-07 20:49:20 -0400

    Update roundFast.jl

--- src/roundFast.jl
+++ src/roundFast.jl
@@ -1,31 +1,31 @@
 
 function (+){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = eftSum2(a,b)
-    eftRound(hi, lo, rounding)
+    hi,lo = add_errorfree(a, b)
+    round_errorfree(hi, lo, rounding)
 end
 
 function (-){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = eftDiff2(a,b)
-    eftRound(hi, lo, rounding)
+    hi,lo = subtract_errorfree(a,b)
+    round_errorfree(hi, lo, rounding)
 end
 
 function (*){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = eftProd2(a,b)
-    eftRound(hi, lo, rounding)
+    hi,lo = multiply_errorfree(a, b)
+    round_errorfree(hi, lo, rounding)
 end
 
 function (/){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = eftDiv2Approx(a,b)
-    eftRound(hi, lo, rounding)
+    hi,lo = divide_accurately(a, b)
+    round_errorfree(hi, lo, rounding)
 end
 
 function (square){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = eftSquare(a,b)
-    eftRound(hi, lo, rounding)
+    hi,lo = square_errorfree(a, b)
+    round_errorfree(hi, lo, rounding)
 end
 
 function (sqrt){T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
-    hi,lo = eftSqrtApprox(a,b)
-    eftRound(hi, lo, rounding)
+    hi,lo = sqrt_accurately(a, b)
+    round_errorfree(hi, lo, rounding)
 end
 
