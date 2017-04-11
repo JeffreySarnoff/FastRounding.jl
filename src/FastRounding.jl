c2f64ba6758d4605ea931bb36c7c057318d6a76f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-11 10:47:52 -0400

    Update FastRounding.jl

--- src/FastRounding.jl
+++ src/FastRounding.jl
@@ -12,54 +12,54 @@ setrounding(Float64, RoundNearest)
 setrounding(Float32, RoundNearest)
 
 
-function +{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function +{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = add_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
-function -{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function -{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = subtract_errorfree(a,b)
     return round_errorfree(hi, lo, rounding)
 end
 
-function *{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function *{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = multiply_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
-function inv{T<:AbstractFloat, R<:RoundingMode}(a::T, rounding::R)
+function inv{T<:SysFloat, R<:RoundingMode}(a::T, rounding::R)::T
     hi, lo = inv_errorfree(a)
     return round_errorfree(hi, lo, rounding)
 end
 
-function /{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function /{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = divide_accurately(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
-function square{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function square{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = square_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
-function sqrt{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function sqrt{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = sqrt_accurately(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
 
-round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:ToZero}) =
+round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:ToZero})::T =
     (signbit(hi)!=signbit(lo) ? next_nearerto_zero(hi) : hi)
 
-round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:FromZero}) =
+round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:FromZero})::T =
     (signbit(hi)==signbit(lo) ? next_awayfrom_zero(hi) : hi)
 
-round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Up}) =
+round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:Up})::T =
     (signbit(lo) ? hi : next_float(hi))
 
-round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Down}) =
+round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:Down})::T =
     (signbit(lo) ? prev_float(hi) : hi)
 
-round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Nearest}) = hi
+round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:Nearest})::T = hi
 
 end # module
