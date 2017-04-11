035bda7e34d9182fdbbcf207420c26abf903c2af
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-11 09:38:36 -0400

    Update FastRounding.jl

--- src/FastRounding.jl
+++ src/FastRounding.jl
@@ -1,5 +1,8 @@
 module FastRounding
 
+export square
+import Base.Math: +, -, *, /, inv, sqrt
+
 using AdjacentFloats
 using ErrorfreeArithmetic
 
@@ -9,27 +12,27 @@ setrounding(Float64, RoundNearest)
 setrounding(Float32, RoundNearest)
 
 
-function Base.:+{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function +{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
     hi, lo = add_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
-function Base.:-{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function -{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
     hi, lo = subtract_errorfree(a,b)
     return round_errorfree(hi, lo, rounding)
 end
 
-function Base.:*{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function *{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
     hi, lo = multiply_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
-function Base.:inv{T<:AbstractFloat, R<:RoundingMode}(a::T, rounding::R)
+function inv{T<:AbstractFloat, R<:RoundingMode}(a::T, rounding::R)
     hi, lo = inv_errorfree(a)
     return round_errorfree(hi, lo, rounding)
 end
 
-function Base.:/{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function /{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
     hi, lo = divide_accurately(a, b)
     return round_errorfree(hi, lo, rounding)
 end
@@ -39,7 +42,7 @@ function square{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
     return round_errorfree(hi, lo, rounding)
 end
 
-function Base.sqrt{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+function sqrt{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
     hi, lo = sqrt_accurately(a, b)
     return round_errorfree(hi, lo, rounding)
 end
