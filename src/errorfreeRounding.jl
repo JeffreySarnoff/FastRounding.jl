4a76aadcdfc18cf83c734ba28955f8cb22a758d6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-11 09:11:36 -0400

    Update errorfreeRounding.jl

--- src/errorfreeRounding.jl
+++ src/errorfreeRounding.jl
@@ -1,63 +1,13 @@
-
-#=
-
-       RoundDown
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   hi              hi
-       +   -   prevfloat(hi)   next_nearerto_zero(hi) == prev_float(hi)
-       -   +   hi              hi
-       -   -   prevfloat(hi)   next_awayfrom_zero(hi) == prev_float(hi)
-
-       RoundUp
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   nextfloat(hi)   next_awayfrom_zero(hi) == next_float(hi)
-       +   -   hi              hi
-       -   +   nextfloat(hi)   next_nearerto_zero(hi) == next_float(hi)
-       -   -   hi              hi
-
-       RoundFromZero
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   nextfloat(hi)   next_awayfrom_zero(hi)
-       +   -   hi              hi
-       -   +   hi              hi
-       -   -   prevfloat(hi)   next_awayfrom_zero(hi)
-
-       RoundToZero
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   hi              hi
-       +   -   prevfloat(hi)   next_nearerto_zero(hi)
-       -   +   nextfloat(hi)   next_nearerto_zero(hi)
-       -   -   hi              hi
-
-      RoundNearest
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   hi              hi
-       +   -   hi              hi
-       -   +   hi              hi
-       -   -   hi              hi
-=#
-
-
-@inline round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:ToZero}) = 
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:ToZero}) =
     (signbit(hi)!=signbit(lo) ? next_nearerto_zero(hi) : hi)
 
-@inline round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:FromZero}) = 
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:FromZero}) =
     (signbit(hi)==signbit(lo) ? next_awayfrom_zero(hi) : hi)
 
-@inline round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Up}) = 
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Up}) =
     (signbit(lo) ? hi : next_float(hi))
 
-@inline round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Down}) = 
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Down}) =
     (signbit(lo) ? prev_float(hi) : hi)
 
-@inline round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Nearest}) = hi
-
-
-
-@inline round_errorfree{T<:AbstractFloat}(hi::T, ::RoundingMode{:Nearest}) = hi
-
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Nearest}) = hi
