17241ba136d0d6fad7eef27a55f5e8a622954f9a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-11 09:17:10 -0400

    Update FastRounding.jl

--- src/FastRounding.jl
+++ src/FastRounding.jl
@@ -8,6 +8,7 @@ const SysFloat = Union{Float64, Float32}  # fast iff fma is available in hardwar
 set_rounding(Float64, RoundNearest)
 set_rounding(Float32, RoundNearest)
 
+
 round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:ToZero}) =
     (signbit(hi)!=signbit(lo) ? next_nearerto_zero(hi) : hi)
 
@@ -23,7 +24,40 @@ round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Down}) =
 round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Nearest}) = hi
 
 
-include("errorfreeRounding.jl")
-include("roundedArithmetic.jl")
+function Base.+{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+    hi, lo = add_errorfree(a, b)
+    return round_errorfree(hi, lo, rounding)
+end
+
+function Base.-{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+    hi, lo = subtract_errorfree(a,b)
+    return round_errorfree(hi, lo, rounding)
+end
+
+function Base.*{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+    hi, lo = multiply_errorfree(a, b)
+    return round_errorfree(hi, lo, rounding)
+end
+
+function Base.inv{T<:AbstractFloat, R<:RoundingMode}(a::T, rounding::R)
+    hi, lo = inv_errorfree(a)
+    return round_errorfree(hi, lo, rounding)
+end
+
+function Base./{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+    hi, lo = divide_accurately(a, b)
+    return round_errorfree(hi, lo, rounding)
+end
+
+function square{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+    hi, lo = square_errorfree(a, b)
+    return round_errorfree(hi, lo, rounding)
+end
+
+function Base.sqrt{T<:AbstractFloat, R<:RoundingMode}(a::T, b::T, rounding::R)
+    hi, lo = sqrt_accurately(a, b)
+    return round_errorfree(hi, lo, rounding)
+end
+
 
 end # module
