1833d05663f9e3ef8ca1a0050beefa516affae92
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-11 09:14:10 -0400

    Update FastRounding.jl

--- src/FastRounding.jl
+++ src/FastRounding.jl
@@ -8,6 +8,21 @@ const SysFloat = Union{Float64, Float32}  # fast iff fma is available in hardwar
 set_rounding(Float64, RoundNearest)
 set_rounding(Float32, RoundNearest)
 
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:ToZero}) =
+    (signbit(hi)!=signbit(lo) ? next_nearerto_zero(hi) : hi)
+
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:FromZero}) =
+    (signbit(hi)==signbit(lo) ? next_awayfrom_zero(hi) : hi)
+
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Up}) =
+    (signbit(lo) ? hi : next_float(hi))
+
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Down}) =
+    (signbit(lo) ? prev_float(hi) : hi)
+
+round_errorfree{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:Nearest}) = hi
+
+
 include("errorfreeRounding.jl")
 include("roundedArithmetic.jl")
 
