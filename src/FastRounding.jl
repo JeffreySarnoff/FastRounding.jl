1bf9fade8355f66cf90ab4443c8cbe3c1024a54c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-12 07:32:46 -0400

    Update FastRounding.jl

--- src/FastRounding.jl
+++ src/FastRounding.jl
@@ -12,48 +12,50 @@ const SysFloat = Union{Float64, Float32}  # fast iff fma is available in hardwar
 setrounding(Float64, RoundNearest)
 setrounding(Float32, RoundNearest)
 
-add_round{T<:SysFloat}(a::T, b::T) = a + b
 function add_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = add_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
+add_round{T<:SysFloat}(a::T, b::T) = a + b
 
-sub_round{T<:SysFloat}(a::T, b::T) = a - b
 function sub_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = subtract_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
+sub_round{T<:SysFloat}(a::T, b::T) = a - b
 
-mul_round{T<:SysFloat}(a::T, b::T) = a * b
 function mul_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = multiply_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
+mul_round{T<:SysFloat}(a::T, b::T) = a * b
 
-inv_round{T<:SysFloat}(a::T) = inv(a)
 function inv_round{T<:SysFloat, R<:RoundingMode}(a::T, rounding::R)::T
     hi, lo = inv_errorfree(a)
     return round_errorfree(hi, lo, rounding)
 end
+inv_round{T<:SysFloat}(a::T) = inv(a)
 
-div_round{T<:SysFloat}(a::T, b::T) = a / b
 function div_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = divide_accurately(a, b)
     return round_errorfree(hi, lo, rounding)
 end
+div_round{T<:SysFloat}(a::T, b::T) = a / b
 
-sqr_round{T<:SysFloat}(a::T) = a^2
 function sqr_round{T<:SysFloat, R<:RoundingMode}(a::T, rounding::R)::T
     hi, lo = square_errorfree(a)
     return round_errorfree(hi, lo, rounding)
 end
+sqr_round{T<:SysFloat}(a::T) = a^2
 
-sqrt_round{T<:SysFloat}(a::T) = sqrt(a)
 function sqrt_round{T<:SysFloat, R<:RoundingMode}(a::T, rounding::R)::T
     hi, lo = sqrt_accurately(a)
     return round_errorfree(hi, lo, rounding)
 end
+sqrt_round{T<:SysFloat}(a::T) = sqrt(a)
 
+
+round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:Nearest})::T = hi
 
 round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:ToZero})::T =
     signbit(hi) != signbit(lo) ? AdjacentFloats.next_nearerto_zero(hi) : hi
@@ -66,8 +68,6 @@ round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:Up})::T =
 
 round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:Down})::T =
     signbit(lo) ? prev_float(hi) : hi
-
-round_errorfree{T<:SysFloat}(hi::T, lo::T, ::RoundingMode{:Nearest})::T = hi
 
     
 end # module
