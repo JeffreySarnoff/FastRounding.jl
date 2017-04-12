8e1a1c574dbce8b34cf07a499c02de0984edc10c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-04-12 01:20:58 -0400

    Update FastRounding.jl

--- src/FastRounding.jl
+++ src/FastRounding.jl
@@ -12,39 +12,45 @@ const SysFloat = Union{Float64, Float32}  # fast iff fma is available in hardwar
 setrounding(Float64, RoundNearest)
 setrounding(Float32, RoundNearest)
 
-
+add_round{T<:SysFloat}(a::T, b::T) = a + b
 function add_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
-    hi, lo = add_errorfree(2*a, b)
+    hi, lo = add_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
+sub_round{T<:SysFloat}(a::T, b::T) = a - b
 function sub_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
-    hi, lo = subtract_errorfree(a,b)
+    hi, lo = subtract_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
+mul_round{T<:SysFloat}(a::T, b::T) = a * b
 function mul_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = multiply_errorfree(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
+inv_round{T<:SysFloat}(a::T) = inv(a)
 function inv_round{T<:SysFloat, R<:RoundingMode}(a::T, rounding::R)::T
     hi, lo = inv_errorfree(a)
     return round_errorfree(hi, lo, rounding)
 end
 
+div_round{T<:SysFloat}(a::T, b::T) = a / b
 function div_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
     hi, lo = divide_accurately(a, b)
     return round_errorfree(hi, lo, rounding)
 end
 
-function sqr_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
-    hi, lo = square_errorfree(a, b)
+sqr_round{T<:SysFloat}(a::T) = a^2
+function sqr_round{T<:SysFloat, R<:RoundingMode}(a::T, rounding::R)::T
+    hi, lo = square_errorfree(a)
     return round_errorfree(hi, lo, rounding)
 end
 
-function sqrt_round{T<:SysFloat, R<:RoundingMode}(a::T, b::T, rounding::R)::T
-    hi, lo = sqrt_accurately(a, b)
+sqrt_round{T<:SysFloat}(a::T) = sqrt(a)
+function sqrt_round{T<:SysFloat, R<:RoundingMode}(a::T, rounding::R)::T
+    hi, lo = sqrt_accurately(a)
     return round_errorfree(hi, lo, rounding)
 end
 
